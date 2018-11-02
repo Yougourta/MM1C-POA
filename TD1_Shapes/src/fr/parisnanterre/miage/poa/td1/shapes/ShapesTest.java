a8f47448df738d8947a25a1408396af0fa780e7b
Yougourta <dy_ait_saada@esi.dz>
2018-11-02 15:02:32 +0100

    Update 1.2

--- TD1_Shapes/src/fr/parisnanterre/miage/poa/td1/shapes/ShapesTest.java
+++ TD1_Shapes/src/fr/parisnanterre/miage/poa/td1/shapes/ShapesTest.java
@@ -3,13 +3,13 @@ package fr.parisnanterre.miage.poa.td1.shapes;
 public class ShapesTest {
 
     public static void main(String[] args) {
-        Point2D point2D = new Point2D(1,1);
-        Square square = new Square(5,5);
-        Rectangle rectangle = new Rectangle(2,10);
+        Shape2D point2D = new Point2D(1,1);
+        Shape2D square = new Square(5,5);
+        Shape2D rectangle = new Rectangle(2,10);
 
-        Point3D point3D = new Point3D(1,1,1);
-        Sphere sphere = new Sphere(5, point3D);
-        RevolutionCone revolutionCone = new RevolutionCone(5, 5, point3D);
+        Shape3D point3D = new Point3D(1,1,1);
+        Shape3D sphere = new Sphere(5, (Point3D) point3D);
+        Shape3D revolutionCone = new RevolutionCone(5, 5, (Point3D) point3D);
 
         System.out.println(square.toString());
         System.out.println(rectangle.toString());
